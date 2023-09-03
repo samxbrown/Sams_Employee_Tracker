16a22fe91494f79bd5d2f889aac76a9159c6b4b3
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-09-02 18:56:40 -0700

    connection.js database connection added

---
+++ Main/db/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pookie1$',
+    database: 'employee_tracker_db'
+});
+
+module.exprts = db;
