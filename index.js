6e90b916fbd3ea211ce75a843a71a50ce6f6ea6d
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-09-06 07:41:02 -0700

    added else if statement to show all roles

--- index.js
+++ index.js
@@ -14,21 +14,23 @@ var employee_tracker = function () {
         name: 'prompt',
         message: 'What would you like to do?',
         choices: ['View All Department', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
-//     }]).then((answers) => {
-//         if (answers.prompt === 'View All Department') {
-//             db.query(`SELECT * FROM department`, (err, result) => {
-//                 if (err) throw err;
-//                 console.log("Viewing All Departments: ");
-//                 console.table(result);
-//                 employee_tracker();
-//             });
-//         } else if (answers.prompt === 'View All Roles') {
-//             db.query(`SELECT * FROM role`, (err, result) => {
-//                 if (err) throw err;
-//                 console.log("Viewing All Roles: ");
-//                 console.table(result);
-//                 employee_tracker();
-//             });
+        // added then prompt to view all departments
+    }]).then((answers) => {
+        if (answers.prompt === 'View All Department') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Departments: ");
+                console.table(result);
+                employee_tracker();
+            });
+            // after then or else if answer prompt is view all roles then grab all role information or throw an error
+        } else if (answers.prompt === 'View All Roles') {
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing All Roles: ");
+                console.table(result);
+                employee_tracker();
+            });
 //         } else if (answers.prompt === 'View All Employees') {
 //             db.query(`SELECT * FROM employee`, (err, result) => {
 //                 if (err) throw err;
